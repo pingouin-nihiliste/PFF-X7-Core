9e9cb9d3349825cbaf28a312c8a4b896ee661e9b
Valentin Bonnard <valentinbonnard@macbook-pro-de-valentin.home>
2022-06-24 22:01:29 +0200

    [CORE] Add pino as logger, proto is working, and add service to test custom logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+import { stdTimeFunctions } from 'pino';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        timestamp: stdTimeFunctions.isoTime,
+        level: process.env.NODE_ENV !== 'production' ? 'trace' : 'info',
+        transport: {
+          target: 'pino-pretty',
+          options: { colorize: true },
+        },
+      },
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
+import { INestMicroservice } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from 'nestjs-pino';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
+const signalsNames: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGHUP'];
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      logger: false,
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50051',
+        package: 'auth',
+        protoPath: join(__dirname, 'lib/proto/auth.proto'),
+      },
+    });
+
+  const logger = app.get(Logger);
+
+  signalsNames.forEach((signalName) =>
+    process.on(signalName, (signal) => {
+      logger.log(`Retrieved signal: ${signal}, application terminated`);
+      process.exit(0);
+    }),
+  );
+
+  process.on('uncaughtException', (error: Error) => {
+    logger.error({ err: error });
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Promise Rejection, reason: ${reason}`);
+    promise.catch((err: Error) => {
+      logger.error({ err });
+      process.exit(1);
+    });
+  });
+  await app.listen(() =>
+    logger.log(`Microservice listening on port:: ${50051}`),
+  );
 }
 bootstrap();
